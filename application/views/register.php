4e89cac9dc5c3e8bfc210177b40ed38c7b1df934
Rafael Ribeiro <rafael.ntw@gmail.com>
2016-06-03 15:20:57 +0100

    Tirar HMVC

--- application/views/register.php
+++ application/views/register.php
@@ -1,29 +1,28 @@
 <!-- Includes -->
 <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://localhost/dw/angular/css/bootstrap.min.css">
 
 <!-- jQuery library -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 <!-- Latest compiled JavaScript -->
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
 <script src="https://cdn.rawgit.com/Nagashitw/dw/master/angular/scripts/login.js"></script>
-<!--Popup -->
+<!--Login Page -->
 <div class="container">
+  <?php echo validation_errors(); ?>
+  <?php echo form_open('form'); ?>
 <div class="row">
 <div class="col-md-4 col-md-offset-4">
   <form name="register" method="post" action="<?php echo site_url('authentication/Register'); ?>">
     <h2>Registar Utilizador</h2>
-    <input type="text" class="user" name="username" placeholder="username">
-    <input type="password" class="pass" name="password" placeholder="password">
-    <input type="email" class="email" name="email" placeholder="Email">
-    <input type="number" class="numero" name="phone" placeholder="Telefone">
-    <button type="submit">Registar</button>
+    <input class="form-control" type="text" class="user" name="username" placeholder="username">
+    <input class="form-control"type="password" class="pass" name="password" placeholder="password">
+    <input class="form-control"type="email" class="email" name="email" placeholder="Email">
+    <input class="form-control"type="number" class="numero" name="phone" placeholder="Telefone">
+    <button class="btn btn-danger"type="submit">Registar</button>
   </form>
 </div>
 </div>
-  <?php echo validation_errors(); ?>
-  <?php echo form_open('form'); ?>
 
 
 </div>
