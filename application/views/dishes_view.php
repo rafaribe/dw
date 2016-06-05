55156022dc4a4431bb80d7da37ac4d7704f80c1c
FilipaTrigo <claudiaftrigo@hotmail.com>
2016-06-05 16:50:35 +0100

    cenas filipa

--- application/views/dishes_view.php
+++ application/views/dishes_view.php
@@ -1,7 +1,8 @@
-<?php include "sample_navbar_view.php" ?>
+<?php include "sample_navbar_view_logout.php" ?>
 <div class="row">
   <div class="col-xs-6 col-md-3">
     <a href="#" class="thumbnail">
       <img src="http://assets.production.postozero.com.s3.amazonaws.com/2013/10/30/21/05/36/348/pazeamor.jpg" alt="...">
     </a>
+    <a href="dish_register" class="btn btn-danger"type="submit">Inserir</a>
   </div>
