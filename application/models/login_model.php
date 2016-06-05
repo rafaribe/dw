f33cf3b130db6cd600bc13f108bfe5eee10ed020
Rafael Ribeiro <rafael.ntw@gmail.com>
2016-06-05 16:29:06 +0100

    Revert "git commit cenas"
    This reverts commit 85de903213d8ead427ceeb9549b311d26d60a998.

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -1,7 +1,27 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Rest_Model extends CI_Model {
+class Login_Model extends CI_Model {
+  function login($username, $password)
+ {
+   $this -> db -> select('id, username, password');
+   $this -> db -> from('users');
+   $this -> db -> where('username', $username);
+   $this -> db -> where('password', MD5($password));
+   $this -> db -> limit(1);
+
+   $query = $this -> db -> get();
+
+   if($query -> num_rows() == 1)
+   {
+     return $query->result();
+   }
+   else
+   {
+     return false;
+   }
+ }
+
   function teste()
     {
         $this->db->select('USER_NAME,USER_PASSWORD');
