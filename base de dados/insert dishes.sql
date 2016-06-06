4237198960446405940de07817e8fba532460000
FilipaTrigo <claudiaftrigo@hotmail.com>
2016-06-06 16:16:53 +0100

    base de dados

---
+++ base de dados/insert dishes.sql
@@ -0,0 +1,233 @@
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Bacalhau a Bras',
+'Peixe',
+'bacalhau_bras.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Cozido a Potuguesa',
+'Carne',
+'cozido.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Ssrdinha Assada com Batata Cozida e Pimentos',
+'Peixe',
+'sardinha.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Creme de Legumes',
+'Sopa',
+'creme_legumes.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Bitoque',
+'Carne',
+'bitoque.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'CheeseCake',
+'Sobremesa',
+'chesse.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Caldo Verde',
+'Sopa',
+'caldo_verde.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Mousse de Chocolate',
+'Sobremesa',
+'mousse.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Pao e Azeitonas',
+'Entrada',
+'azeitonas.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Vinho Tinto',
+'Bebida',
+'tinto.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Tabuleiro de Queijos',
+'Entrada',
+'queijos.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Vinho Branco',
+'Bebida',
+'branco.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Agua',
+'Bebida',
+'agua.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Sumo de Laranja',
+'Bebida',
+'sumo.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Cafe',
+'Bebida',
+'cafe.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Morangos',
+'Fruta',
+'morango.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Hambuger com carne e salada',
+'Fast-Food',
+'fast.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Francesinha',
+'Fast-Food',
+'francesinha.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Grelhada Mista',
+'Carne',
+'mista.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Martini',
+'Aperitivo',
+'drink.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Banana',
+'Fruta',
+'banana.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Salada Mista',
+'Salada',
+'salada_mista.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Salada de Fruta',
+'Sobremesa',
+'salada_fruta.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Cafe com Maceira',
+'Bebida',
+'bagacu.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Salada de Alface',
+'Salada',
+'alface.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Salada de Cenoura',
+'Salada',
+'cenoura.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Salada de Tomate',
+'Salada',
+'tomate.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Salada de Tomate e Alface',
+'Salada',
+'al_to.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Salada de Pepino',
+'Salada',
+'pepino.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Bola de Gelado',
+'Sobremesa',
+'gelado.png'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Carbonara',
+'Carne',
+'carbonara.jpg'
+);
+
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Peixe com batata cozida',
+'Peixe',
+'ovo.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Legumes Saltiados',
+'Vegetariano',
+'legumes.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Legumes Cozidos',
+'Vegetariano',
+'leg_cozidos.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Feijoada',
+'Carne',
+'feijoada.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Rancho',
+'Carne',
+'rancho.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Bacalhau a Gomes de Sa',
+'Peixe',
+'bacalhau_gomes.jpg'
+);
+INSERT INTO Dishes (DISH_NAME,DISH_TYPE,DISH_IMAGE)
+values(
+'Bacalhau com Broa',
+'Peixe',
+'bacalhau_broa.jpg'
+);
+commit;
+
+
+
---
+++ base de dados/inserts menu.sql
@@ -0,0 +1,84 @@
+INSERT INTO Menus (RESTAURANT_ID, MENU_NAME)
+values(
+'1',
+'Menu Tia Iva'
+);
+INSERT INTO Menus (RESTAURANT_ID, MENU_NAME)
+values(
+'2',
+'Menu Tosco'
+);
+
+INSERT INTO Menus (RESTAURANT_ID, MENU_NAME)
+values(
+'3',
+'Menu Arouqueza'
+);
+INSERT INTO Menus (RESTAURANT_ID, MENU_NAME)
+values(
+'4',
+'Menu Maionese'
+);
+INSERT INTO Menus (RESTAURANT_ID, MENU_NAME)
+values(
+'5',
+'Menu Churrasqueira'
+);
+INSERT INTO Menus (RESTAURANT_ID, MENU_NAME)
+values(
+'6',
+'Menu Vintage'
+);
+INSERT INTO Menus (RESTAURANT_ID, MENU_NAME)
+values(
+'7',
+'Menu Cortico'
+);
+INSERT INTO Menus (RESTAURANT_ID, MENU_NAME)
+values(
+'8',
+'Menu Tito'
+);
+INSERT INTO Menus (RESTAURANT_ID, MENU_NAME)
+values(
+'9',
+'Menu Budega'
+);
+INSERT INTO Menus (RESTAURANT_ID, MENU_NAME)
+values(
+'10',
+'Menu Casa'
+);
+INSERT INTO Menus (RESTAURANT_ID, MENU_NAME)
+values(
+'11',
+'Menu CB'
+);
+INSERT INTO Menus (RESTAURANT_ID, MENU_NAME)
+values(
+'12',
+'Menu Forno'
+);
+INSERT INTO Menus (RESTAURANT_ID, MENU_NAME)
+values(
+'13',
+'Menu Martelo'
+);
+
+INSERT INTO Menus (RESTAURANT_ID, MENU_NAME)
+values(
+'14',
+'Menu Porta'
+);
+INSERT INTO Menus (RESTAURANT_ID, MENU_NAME)
+values(
+'15',
+'Menu Cacimbo'
+);
+INSERT INTO Menus (RESTAURANT_ID, MENU_NAME)
+values(
+'16',
+'Menu Mac'
+);
+
+commit;
