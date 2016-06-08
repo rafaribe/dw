2a7685d26485ec6805f89cfe52669aa4e61d47c7
FilipaTrigo <claudiaftrigo@hotmail.com>
2016-06-08 11:44:49 +0100

    inserts dish done

---
+++ base de dados/inserts prices.sql
@@ -0,0 +1,42 @@
+insert into Prices (price_id, price_value)
+values('1','5');
+insert into Prices (price_id, price_value)
+values('2','10');
+insert into Prices (price_id, price_value)
+values('3','15');
+insert into Prices (price_id, price_value)
+values('4','20');
+insert into Prices (price_id, price_value)
+values('5','25');
+insert into Prices (price_id, price_value)
+values('6','3');
+insert into Prices (price_id, price_value)
+values('7','6');
+insert into Prices (price_id, price_value)
+values('8','9');
+insert into Prices (price_id, price_value)
+values('9','12');
+insert into Prices (price_id, price_value)
+values('10','15');
+insert into Prices (price_id, price_value)
+values('11','4');
+insert into Prices (price_id, price_value)
+values('12','8');
+insert into Prices (price_id, price_value)
+values('13','16');
+insert into Prices (price_id, price_value)
+values('14','24');
+insert into Prices (price_id, price_value)
+values('15','2');
+insert into Prices (price_id, price_value)
+values('16','4');
+insert into Prices (price_id, price_value)
+values('17','26');
+insert into Prices (price_id, price_value)
+values('18','14');
+insert into Prices (price_id, price_value)
+values('19','30');
+insert into Prices (price_id, price_value)
+values('20','35');
+
+commit;
