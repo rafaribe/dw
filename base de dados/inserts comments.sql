dd56bff3a70ff60e79202c16d07cbd7ebca22fbe
FilipaTrigo <claudiaftrigo@hotmail.com>
2016-06-08 12:15:40 +0100

    comments

---
+++ base de dados/inserts comments.sql
@@ -0,0 +1,4 @@
+INSERT INTO Comments (comment_id,user_id, COMMENT_CREATION_DATE, comment_text)
+values(
+'1','1',sysdate, ' Este restaurante recomenda-se, pois tem um optimo funcionamento e a qualidade e muito boa'
+);
