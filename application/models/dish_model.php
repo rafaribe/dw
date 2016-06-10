8c51f3b4e7ed07af778a208e3cfdb5959f11c6e7
Rafael Ribeiro <rafael.ntw@gmail.com>
2016-06-10 12:41:13 +0100

    base
    base

--- application/models/dish_model.php
+++ application/models/dish_model.php
@@ -2,12 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Dish_Model extends CI_Model
 {
-  function teste()
-    {
-        $this->db->select('USER_NAME,USER_PASSWORD');
-        $result = $this->db->get('USERS');
-        return $result->result();
-    }
+
     function  dish_id_name_image()
       {
           $this->db->select('DISH_ID,DISH_NAME, DISH_IMAGE');
