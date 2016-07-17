5ef25c1c22d6b82d69e39fbe3a0d44919df4fed0
Rafael Ribeiro <rafael.ntw@gmail.com>
2016-07-17 21:51:48 +0100

    a
    a

--- database_files/script a dar.sql
+++ database_files/script a dar.sql
@@ -1862,22 +1862,19 @@ _SEQ.NEXTVAL||'</DISH_ID>
 INSERT INTO DISHES VALUES(DISHES_SEQ.NEXTVAL,dish_name,dish_type,dish_image);
 END;*/
 
-create or replace PROCEDURE insert_xml_proc (
-dish_id IN NUMBER,
+CREATE OR REPLACE PROCEDURE insert_xml_proc (
 dish_name IN VARCHAR,
 dish_type IN VARCHAR,
 dish_image IN VARCHAR ) AS
 BEGIN
-select COUNT(*)+1 into dish_id FROM DISHES_XML;
-INSERT INTO DISHES_XML
- values (
-xmlelement("xml",
-   xmlelement("item",
-      xmlelement("DISH_ID", dish_id),
+INSERT INTO DISHES_XML values (
+xmlelement("xml", 
+   xmlelement("item", 
+      xmlelement("DISH_ID", (SELECT COUNT(*)+1 FROM DISHES_XML),
       xmlelement("DISH_NAME", dish_name),
       xmlelement("DISH_TYPE", dish_type),
       xmlelement("DISH_IMAGE", dish_image)
    )
 )
-);
+));
 End;
